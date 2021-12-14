f6d3a0801869abef7152f6b6a068d89caf7296cd
David Gaitan <jdavid.gaitan@gmail.com>
2021-12-13 21:49:14 -0600

    adding order status enum

---
+++ app/Enums/OrderStatus.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Enums;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class OrderStatus extends Enum
+{
+    private const PENDING = 'pending';
+    private const PROCESSING = 'processing';
+    private const ON_HOLD = 'on-hold';
+    private const COMPLETED = 'completed';
+    private const CANCELLED = 'cancelled';
+    private const REFUNDED = 'refunded';
+    private const FAILED = 'failed';
+    private const TRASH = 'trash';
+
+    public static function default() {
+        return self::PENDING();
+    }
+}
