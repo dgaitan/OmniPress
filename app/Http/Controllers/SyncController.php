e7d5f07cc888e47282b777eb6e3bd94fb013dae5
David Gaitan <jdavid.gaitan@gmail.com>
2022-01-19 23:15:41 -0600

    quick fix on sync

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -15,7 +15,7 @@ class SyncController extends Controller
     public function index() {
         return Inertia::render('Sync/Index', [
             'syncs' => SyncResource::collection(
-                Sync::with('users')->take(10)->orderBy('id', 'desc')->get()
+                Sync::with('user')->take(10)->orderBy('id', 'desc')->get()
             )
         ]);
     }
@@ -29,7 +29,7 @@ class SyncController extends Controller
         $request->validateWithBag('syncForm', [
             'content_type' => ['required', new SyncContentType],
             'description' => ['max:500']
-        ]);        
+        ]);      
 
         $sync = Sync::create([
             'name' => sprintf('%s sync', ucwords($request->content_type)),
@@ -47,17 +47,28 @@ class SyncController extends Controller
         );
         $sync->logs()->create(['description' => $firstSyncLog]);
 
+        try {
+            $task = new WooCommerceTask;
+            $task->_sync($request->content_type);
 
-        $task = new WooCommerceTask;
-        $task->_sync($request->content_type);
+            $sync->status = Sync::COMPLETED;
+            $sync->save();
 
-        $sync->status = Sync::COMPLETED;
-        $sync->save();
+            $completedLog = sprintf(
+                'Sync completed successfuly at %s', Carbon::now()->format('F j, Y @ H:i:s')
+            );
+            $sync->logs()->create(['description' => $completedLog]);
+        } catch (Exception $e) {
+            $sync->logs()->create([
+                'description' => sprintf('%s sync failed', ucfirst($request->content_type));
+            ]);
+            $sync->logs()->create([
+                'description' => sprintf('Error: %s', $e->getMessage());
+            ]);
 
-        $completedLog = sprintf(
-            'Sync completed successfuly at %s', Carbon::now()->format('F j, Y @ H:i:s')
-        );
-        $sync->logs()->create(['description' => $completedLog]);
+            $sync->status = Sync::FAILED;
+            $sync->save();
+        }
 
         return redirect(route('kinja.sync.index'))
             ->with('message', 'Sync Completed!');
