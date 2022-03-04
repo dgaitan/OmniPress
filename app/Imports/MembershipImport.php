f282ee3ce402546de8f0c39f523e38f252e7d4c8
David Gaitan <jdavid.gaitan@gmail.com>
2022-03-03 19:59:05 -0600

    Cleaning code

--- app/Imports/MembershipImport.php
+++ app/Imports/MembershipImport.php
@@ -3,16 +3,10 @@
 namespace App\Imports;
 
 use App\Services\WooCommerce\Factories\MembershipFactory;
-use App\Models\KindCash;
-use App\Models\Membership;
-use App\Models\WooCommerce\Customer;
-use App\Models\WooCommerce\Product;
-use App\Models\WooCommerce\Order;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\WithProgressBar;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Carbon\Carbon;
 
 class MembershipImport implements ToModel, WithProgressBar, WithHeadingRow
 {
