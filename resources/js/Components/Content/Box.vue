b8945e3dd4cfec62ee4357591c7ba0f555b35552
David Gaitan <jdavid.gaitan@gmail.com>
2022-03-23 12:12:52 -0600

    Fixing Box

--- resources/js/Components/Content/Box.vue
+++ resources/js/Components/Content/Box.vue
@@ -4,7 +4,7 @@
             <h3 class="mr-2 text-xl font-medium">
                 {{ headline }}
             </h3>
-            <div v-if="button">
+            <div v-if="button.link">
                 <Link :href="button.link" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition">
                     {{ button.label }}
                 </Link>
@@ -28,7 +28,10 @@
             },
             button: {
                 type: Object,
-                default: ''
+                default: {
+                    link: null,
+                    label: null
+                }
             }
         },
 
