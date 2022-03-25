6c707d85731f600a18420a89c104230eaf455879
David Gaitan <jdavid.gaitan@gmail.com>
2022-03-25 15:58:10 -0600

    .

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -87,7 +87,7 @@
                                 </td>
 
                                 <!-- Actions -->
-                                <td class="font-medium">
+                                <td class="font-medium px-2">
                                     <jet-dropdown align="right" width="48">
                                         <template #trigger>
                                             <button type="button" class="inline-flex items-center px-3 py-2 border border-gray-200 text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
