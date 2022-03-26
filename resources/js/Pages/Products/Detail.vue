64385592b066f7468e10711ca3d25afe5372e4e0
David Gaitan <jdavid.gaitan@gmail.com>
2022-03-25 19:56:24 -0600

    adding initial product detail

--- resources/js/Pages/Products/Detail.vue
+++ resources/js/Pages/Products/Detail.vue
@@ -1,10 +1,56 @@
 <template>
-
+    <Layout :title="pageTitle">
+        <Container>
+            <Row>
+                <Column :mdSize="6">
+                    <ProductIntro :product="product.data" />
+                </Column>
+                <Column :mdSize="6" class="text-right">
+                    <Button size="md" @click="goToStore()">SEE ON KINDHUMANS.COM</Button>
+                </Column>
+            </Row>
+            <Row>
+                <Column :mdSize="8">
+                    
+                </Column>
+                <Column :mdSize="4"></Column>
+            </Row>
+        </Container>
+    </Layout>
 </template>
 <script>
 import { defineComponent } from 'vue'
+import Layout from '@/Layouts/Layout.vue'
+import Container from '@/Components/Layouts/Container.vue'
+import Row from '@/Components/Layouts/Row.vue'
+import Column from '@/Components/Layouts/Column.vue'
+import ProductIntro from './Partials/ProductIntro.vue'
+import Button from '@/Components/Button.vue'
 
 export default defineComponent({
+    props: {
+        product: Object
+    },
 
+    components: {
+        Layout,
+        Container,
+        Row,
+        Column,
+        Button,
+        ProductIntro,
+    },
+
+    computed: {
+        pageTitle() {
+            return this.product.data.name;
+        }
+    },
+
+    methods: {
+        goToStore() {
+            window.open(this.product.data.store_permalink, '_blank').focus();
+        }
+    }
 })
 </script>
