b55873449fb0055192875936f048aa4adffdbb09
David Gaitan <jdavid.gaitan@gmail.com>
2022-05-27 15:50:32 -0600

    Getting started with categories controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+
+    }
 }
