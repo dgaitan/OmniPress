a06af541eda6ea53f77f2b6e0f5b590cd2c73dad
David Gaitan <jdavid.gaitan@gmail.com>
2022-07-07 10:17:54 -0600

    Adding testing for printforia when order has a customer

--- tests/Feature/Actions/PrintforiaActionsTest.php
+++ tests/Feature/Actions/PrintforiaActionsTest.php
@@ -5,8 +5,11 @@ namespace Tests\Feature\Actions;
 use App\Actions\Printforia\MaybeCreatePrintforiaOrderAction;
 use App\Actions\WooCommerce\Orders\SyncOrderLineItemProductsAction;
 use App\Jobs\Pritnforia\MaybeCreatePrintforiaOrderJob;
+use App\Jobs\WooCommerce\Orders\SyncOrderLineItemProductsJob;
 use App\Models\Printforia\PrintforiaOrder;
+use App\Models\WooCommerce\Customer;
 use App\Models\WooCommerce\Order;
+use App\Models\WooCommerce\Product;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Notification;
@@ -19,7 +22,7 @@ class PrintforiaActionsTest extends BaseAction
 
         Http::fake([
             $this->getUrl(endpoint: 'orders/550013') => Http::response(
-                body: $this->fixture(name: 'WooCommerce/Printforia/OrderWithGuestCustomer'),
+                body: $this->fixture(name: 'WooCommerce/Printforia/OrderWithCustomer'),
                 status: 200
             ),
             $this->getUrl(endpoint: 'payment_gateways/kindhumans_stripe_gateway') => Http::response(
@@ -68,6 +71,82 @@ class PrintforiaActionsTest extends BaseAction
         $this->assertEquals('Bl2gAKAJuW9dPqiKxndwK', $order->printforiaOrder->printforia_order_id);
         $this->assertEquals('order-550013', $order->printforiaOrder->customer_reference);
         $this->assertEquals(1, $order->printforiaOrder->items()->count());
+    }
+
+    public function test_maybe_create_printforia_order_action_with_customer()
+    {
+        $this->disableScout();
+
+        Http::fake([
+            $this->getUrl(endpoint: 'orders/550013') => Http::response(
+                body: $this->fixture(name: 'WooCommerce/Printforia/OrderWithCustomer'),
+                status: 200
+            ),
+            $this->getUrl(endpoint: 'payment_gateways/kindhumans_stripe_gateway') => Http::response(
+                body: $this->fixture(name: 'WooCommerce/PaymentMethodDetail'),
+                status: 200
+            ),
+            $this->getUrl(endpoint: 'causes*') => Http::response(
+                body: $this->fixture(name: 'causesList'),
+                status: 200
+            ),
+            $this->getUrl(endpoint: 'products/549943') => Http::response(
+                body: $this->fixture(name: 'WooCommerce/Printforia/PrintforiaProduct'),
+                status: 200
+            ),
+            $this->getUrl(endpoint: 'customers/2202') => Http::response(
+                body: $this->fixture(name: 'WooCommerce/Printforia/Customer'),
+                status: 200
+            ),
+            $this->getPrintforiaUrl(endpoint: 'orders') => Http::response(
+                body: $this->fixture(name: 'Printforia/PrintforiaOrderWithCustomer'),
+                status: 200
+            ),
+            $this->getPrintforiaUrl(endpoint: 'orders/Bl2gAKAJuW9dPqiKxndwK') => Http::response(
+                body: $this->fixture(name: 'Printforia/PrintforiaOrderWithCustomer'),
+                status: 200
+            )
+        ]);
+
+        Notification::fake();
+        Bus::fake([
+            MaybeCreatePrintforiaOrderJob::class
+        ]);
+
+        $api = $this->get_woocommerce_service();
+        $api->causes()->collectAndSync();
+        $api->products()->getAndSync(id: 549943);
+        $api->customers()->getAndSync(id: 2202);
+        $api->paymentMethods()->getAndSync(id: 'kindhumans_stripe_gateway');
+        $api->orders()->getAndSync(id: 550013);
+
+        // Woo Product Must be a printforia product
+        $product = Product::whereProductId(549943)->first();
+        $this->assertTrue($product->is_printforia === 1); // One bc it stores an integer in test
+
+        $order = Order::whereOrderId(550013)->first();
+        $customer = Customer::whereCustomerId(2202)->first();
+
+        Bus::assertDispatched(MaybeCreatePrintforiaOrderJob::class);
+
+        SyncOrderLineItemProductsAction::run($order);
+        MaybeCreatePrintforiaOrderAction::run($order);
+
+        $this->assertEquals(1, $order->items->count());
+        $this->assertTrue(! is_null($order->printforiaOrder));
+        $this->assertInstanceOf(PrintforiaOrder::class, $order->printforiaOrder);
+        $this->assertEquals('Bl2gAKAJuW9dPqiKxndwK', $order->printforiaOrder->printforia_order_id);
+        $this->assertEquals('order-550013', $order->printforiaOrder->customer_reference);
+        $this->assertEquals(1, $order->printforiaOrder->items()->count());
+
+        // Order must have a customer
+        $this->assertInstanceOf(Customer::class, $order->customer);
+        $this->assertEquals($customer->customer_id, $order->customer->customer_id);
+        $this->assertEquals($customer->email, $order->customer->email);
+
+        // Testing PRintforia Order
+        $this->assertEquals(1, $order->printforiaOrder->items->count());
+        $this->assertEquals(2, $order->printforiaOrder->items->first()->quantity);
     }
 
     protected function getPrintforiaUrl(string $endpoint): string
