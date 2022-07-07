2a58ced196eb7cabdbad515c10ac1ce87d65f4ef
David Gaitan <jdavid.gaitan@gmail.com>
2022-07-06 19:44:52 -0600

    adding tests for order donations

--- tests/Feature/Actions/DonationActionsTest.php
+++ tests/Feature/Actions/DonationActionsTest.php
@@ -2,11 +2,10 @@
 
 namespace Tests\Feature\Actions;
 
-use App\Jobs\Donations\AssignOrderDonationJob;
 use App\Models\Causes\Cause;
+use App\Models\Causes\OrderDonation;
 use App\Models\WooCommerce\Order;
 use App\Services\WooCommerce\WooCommerceService;
-use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Http;
 use Tests\Feature\Actions\BaseAction;
@@ -16,7 +15,7 @@ class DonationActionsTest extends BaseAction
     public function test_assign_order_donations(): void
     {
         Http::fake([
-            $this->getUrl(endpoint: 'orders/408177') => Http::response(
+            $this->getUrl(endpoint: 'orders/418136') => Http::response(
                 body: $this->fixture('WooCommerce/OrderDonationCause'),
                 status: 200
             ),
@@ -34,23 +33,38 @@ class DonationActionsTest extends BaseAction
             )
         ]);
 
-        Bus::fake([
-            AssignOrderDonationJob::class
-        ]);
         Notification::fake();
 
         $api = WooCommerceService::make();
         $api->causes()->collectAndSync();
         $api->paymentMethods()->getAndSync('kindhumans_stripe_gateway');
         $api->customers()->getAndSync(2064);
-        $api->orders()->getAndSync(408177);
+        $api->orders()->getAndSync(418136);
 
-        Bus::assertDispatched(AssignOrderDonationJob::class);
-
-        $cause = Cause::whereCauseId(102529)->first();
-        $order = Order::whereOrderId(408177)->first();
+        $cause = Cause::whereCauseId(581)->first();
+        $order = Order::whereOrderId(418136)->first();
 
         $this->assertEquals($cause, $order->getCause());
-        $this->assertEquals();
+        $this->assertEquals(123, $order->donations->sum('amount'));
+        $this->assertEquals('$1.23', $order->totalDonated()->formated());
+        $this->assertInstanceOf(OrderDonation::class, $order->donations->first());
+        $this->assertEquals(1, $order->donations->count());
+
+        $donations = $order->donations->map(function ($donation) {
+            return [
+                'id' => $donation->id,
+                'cause' => [
+                    'id' => $donation->cause->id,
+                    'name' => $donation->cause->name,
+                    'type' => $donation->cause->getCauseType(),
+                ],
+                'amount' => [
+                    'value' => $donation->amount,
+                    'format' => $donation->getMoneyValue('amount')->format(),
+                ],
+            ];
+        });
+
+        $this->assertEquals($donations, $order->getDonations());
     }
 }
