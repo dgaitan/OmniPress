1b5d2a513418f5e3f0ac168ed9f77c63d1e7fe1b
David Gaitan <jdavid.gaitan@gmail.com>
2022-07-08 13:18:53 -0600

    Fixing printforia order view

--- resources/js/Pages/Orders/Partials/PrintforiaOrderInfo.vue
+++ resources/js/Pages/Orders/Partials/PrintforiaOrderInfo.vue
@@ -1,6 +1,10 @@
 <template>
     <Box headline="Information">
         <Column class="mb-5">
+            <Label>Kindhumans Order ID</Label>
+            <Link class="text-cyan-500 font-medium" :href="order.order.permalink">#{{ order.order.order_id }}</Link>
+        </Column>
+        <Column class="mb-5">
             <Label>Customer Email</Label>
             <a class="text-cyan-500 font-medium" :href="`mailto:${order.ship_to_address.email}`">{{ order.ship_to_address.email }}</a>
         </Column>
@@ -11,6 +15,14 @@
         <Column :mdSize="6" class="mb-3">
             <Label>Return to address</Label>
             <div class="text-sm text-gray-600" v-html="order.return_to_address_formatted"></div>
+        </Column>
+        <Column class="mb-5" v-if="order.carrier">
+            <Label>Shipping Carrier</Label>
+            {{ order.carrier }}
+        </Column>
+        <Column class="mb-5" v-if="order.tracking_url && order.tracking_number">
+            <Label>Tracking Number</Label>
+            <a class="text-cyan-500 font-medium" :href="order.tracking_url">{{ order.tracking_number }}</a>
         </Column>
     </Box>
     <Box headline="Notes">
@@ -25,6 +37,7 @@
 </template>
 <script>
 import { defineComponent } from 'vue'
+import { Link } from '@inertiajs/inertia-vue3';
 import Box from '@/Components/Content/Box.vue'
 import Column from '@/Components/Layouts/Column.vue'
 import Label from '@/Components/Content/Label.vue'
@@ -39,7 +52,8 @@ export default defineComponent({
         Box,
         Column,
         Label,
-        ClockIcon
+        ClockIcon,
+        Link
     }
 })
 </script>
