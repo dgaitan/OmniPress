68309fdfdd7b9fbe17ea429b2de970160d0550f8
David Gaitan <jdavid.gaitan@gmail.com>
2022-09-06 08:39:10 -0600

    Chaning new membership test name

--- tests/Feature/Http/API/MembershipControllerTest.php
+++ tests/Feature/Http/API/MembershipControllerTest.php
@@ -13,7 +13,6 @@ use App\Models\WooCommerce\Product;
 use App\Services\WooCommerce\WooCommerceService;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Queue;
 
 beforeEach(function () {
@@ -69,7 +68,7 @@ beforeEach(function () {
     $api->orders()->getAndSync(549799);
 });
 
-it('new endpoint should create a new membership', function () {
+it('should create a new membership when call new membership endpoint', function () {
     Queue::fake([
         NewMembershipJob::class
     ]);
@@ -164,140 +163,3 @@ it('new endpoint should create a new membership', function () {
 
     Queue::assertPushed(NewMembershipJob::class);
 })->group('memberships');
-
-// class MembershipControllerTest extends BaseHttp
-// {
-//     use InteractsWithScout;
-
-//     protected function setUp(): void {
-//         parent::setUp();
-
-//         $this->disableScout();
-//         $this->prepareData();
-//     }
-
-//     public function test_new_endpoint_should_create_a_new_membership(): void
-//     {
-//         $customer = Customer::whereEmail('ram@ram.com')->first();
-//         $this->assertNotNull($customer);
-
-//         $membershipProduct = Product::whereProductId(160768)->first();
-//         $this->assertNotNull($membershipProduct);
-
-//         $params = [
-//             'price' => 3500,
-//             'customer_id' => 2064,
-//             'email' => 'ram@ram.com',
-//             'username' => 'David ram',
-//             'order_id' => 549799,
-//             'points' => 750,
-//             'gift_product_id' => 544443,
-//             'product_id' => 160768,
-//         ];
-
-//         $this->actingAs($user = User::factory()->withPersonalTeam()->create());
-
-//         $response = $this->post('api/v1/memberships/new', $params);
-
-//         $response->assertOk();
-//         $response->assertStatus(200);
-
-//         $membership = Membership::whereCustomerId($customer->id)
-//             ->first();
-
-//         $response->assertJson([
-//             'membership' => [
-//                 'id' => 1,
-//                 'start_at' => $membership->start_at->toJSON(),
-//                 'end_at' => $membership->end_at->toJSON(),
-//                 'status' => 'active',
-//                 'shipping_status' => 'pending'
-//             ],
-//             'kind_cash' => [
-//                 'points' => 750,
-//                 'last_earned' => 750
-//             ]
-//         ]);
-
-//         // Testing membership flags after creating membership
-//         $this->assertTrue($membership->isActive());
-//         $this->assertEquals($customer, $membership->customer);
-
-//         // Testing that membership has a kindcash and it has values
-//         $this->assertInstanceOf(KindCash::class, $membership->kindCash);
-//         $this->assertEquals(750, $membership->kindCash->points);
-//         $this->assertEquals(750, $membership->kindCash->last_earned);
-
-//         // Testing that order is related to the latest membership
-//         $order = Order::whereOrderId(549799)->first();
-//         $this->assertTrue($order->has_membership);
-//         $this->assertEquals($order->membership_id, $membership->id);
-//         $this->assertSame($order, $membership->getCurrentOrder());
-
-//         /**
-//          * A gitftproduct is a product that comes with the membership
-//          * and it should be present in the request and it should be preset
-//          * in the order and in the membership
-//          */
-//         $giftProduct = Product::whereProductId(544443)->first();
-//         $this->assertSame($giftProduct, $membership->getCurrentGiftProduct());
-
-//     }
-
-//     /**
-//      * Load Orders and Customer to test memberships
-//      *
-//      * @return void
-//      */
-//     protected function prepareData(): void
-//     {
-//         PaymentMethod::create([
-//             'payment_method_id' => 'kindhumans_stripe_gateway',
-//             'title' => 'Credit Card',
-//             'order' => 1,
-//             'enabled' => true,
-//             'method_title' => 'Kindhumans Payment Gateway',
-//         ]);
-
-//         Customer::create([
-//             'customer_id' => 2064,
-//             'email' => 'ram@ram.com',
-//             'first_name' => 'John',
-//             'last_name' => 'Bar',
-//             'role' => 'customer',
-//             'username' => 'smlueker',
-//             'billing' => '{}',
-//             'shipping' => '{}',
-//             'is_paying_customer' => false,
-//         ]);
-
-//         $api = WooCommerceService::make();
-
-//         Http::fake([
-//             $this->getUrl('products/544443') => Http::response(
-//                 body: $this->fixture('Memberships/GiftProduct'),
-//                 status: 200
-//             ),
-//             $this->getUrl('products/160768') => Http::response(
-//                 body: $this->fixture('Memberships/MembershipProduct'),
-//                 status: 200
-//             ),
-//         ]);
-
-//         $api->products()->getAndSync(160768);
-//         $api->products()->getAndSync(544443);
-
-//         Http::fake([
-//             $this->getUrl(endpoint: 'orders/549799') => Http::response(
-//                 body: $this->fixture('Memberships/Order'),
-//                 status: 200
-//             ),
-//             $this->getUrl(endpoint: 'customers/2064') => Http::response(
-//                 body: $this->fixture('WooCommerce/CustomerDetail'),
-//                 status: 200
-//             ),
-//         ]);
-
-//         $api->orders()->getAndSync(549799);
-//     }
-// }
